4b511e91413fae9ac2cff3c75ed683d2f42e5380
MutationalNeighborhoods <91388592+MutationalNeighborhoods@users.noreply.github.com>
2022-10-28 14:18:01 -0700

    Delete classes directory

--- classes/Food.py
+++
@@ -1,9 +0,0 @@
-from enums.Cell import *
-from enums.Angle import *
-from enums.Rotation import *
-from enums.Thick import *
-import classes.Cell as c
-
-class Food(c.Cell):
-    def __init__(self, x, y, ownerBoard, angleType=AngleType.na, rotationType=RotationType.na, thickType=ThickType.na, active=False):
-        super().__init__(x, y, CellType.food, ownerBoard)
