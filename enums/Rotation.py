99c5bfb2901c48a8213be9c077d4f3d608b21cb1
MutationalNeighborhoods <91388592+MutationalNeighborhoods@users.noreply.github.com>
2022-10-28 14:18:39 -0700

    Delete enums directory

--- enums/Rotation.py
+++
@@ -1,8 +0,0 @@
-import enum
-
-class RotationType(enum.Enum):    
-    up = 0
-    right = 2
-    down = 4
-    left = 6
-    na = "x"
--- enums/Thick.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-class ThickType(enum.Enum):
-    skinny = 0
-    normal = 1
-    wide = 2
-    na = "x"
